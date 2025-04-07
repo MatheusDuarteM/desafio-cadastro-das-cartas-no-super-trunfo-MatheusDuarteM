e080df815d6ec7fed1c89f7fe01743a73d4f40ca
Matheus Duarte Martins <144532351+MatheusDuarteM@users.noreply.github.com>
2025-04-07 14:23:07 +0000

    Foi realizado o exercicio novato do CartasSuperTrunfo onde utilizei o For

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,33 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-    return 0;
+    //escolhi utilizar o for para não precisar replicar o codigo novamente ai depois de por os dados e mostrado como foi pedido
+    for(int i = 1; i < 3; i++){
+        char estado;
+        char codCarta[3];
+        char nomeDaCidade[50];
+        int populacao;
+        float area;
+        float pib;
+        int pontosTuristicos;
+        
+        printf("Carta %d\n",i);
+        printf("Qual o estado:");
+        scanf(" %c",&estado);
+        printf("Qual o codigo:");
+        scanf("%3s",&codCarta);
+        printf("Qual o nome da cidade:");
+        scanf("%s",&nomeDaCidade);
+        printf("Tamanho da população:");
+        scanf("%d",&populacao);
+        printf("O tamanho da Área:");
+        scanf("%f",&area);
+        printf("O PIB:");
+        scanf("%f",&pib);
+        printf("Numero de pontos turisticos:");
+        scanf("%d",&pontosTuristicos);
+        printf("Carta %d: \n Estado: %c\n Código: %c%s\n Nome da cidade: %s\n População: %d\n Área: %f km²\n PIB: %f bilhões de reais\n Número de Pontos Turísticos: %d",i,estado,estado,codCarta,nomeDaCidade,populacao,area,pib,pontosTuristicos);
+        printf("\n\n");
+        }
+        return 0;
 }
